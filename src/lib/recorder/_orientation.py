2768bc5ace68b505dfd9f76540713e6d0cf59e80
SatooRu65536 <ryuto65536@gmail.com>
2025-08-18 14:24:48 +0900

    fix: 不要なインポートとコンストラクタを削除

--- src/lib/recorder/_orientation.py
+++ src/lib/recorder/_orientation.py
@@ -1,4 +1,3 @@
-from logging import Logger
 from typing import Protocol, final
 from src.lib.safelist._safelist import SafeList
 from src.type import QOrientationWithTimestamp
@@ -15,9 +14,6 @@ class OrientationDataRecorderProtocol(Protocol):
 
 class OrientationDataRecorder:
     orientations: SafeList[QOrientationWithTimestamp] = SafeList()
-
-    def __init__(self, trial_id: str, logger: Logger):
-        pass
 
     @final
     def last_orientation(self) -> QOrientationWithTimestamp:
