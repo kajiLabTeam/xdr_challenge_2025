c3ce1d16fbff8bc5d635c260b34ec28560a227fa
SatooRu65536 <ryuto65536@gmail.com>
2025-08-22 08:30:58 +0900

    refactor: estimate_pdrメソッドの戻り値をNoneからPositionに変更

--- src/lib/localizer/pdr.py
+++ src/lib/localizer/pdr.py
@@ -8,10 +8,4 @@ class PDRLocalizer(DataRecorderProtocol, Protocol):
     PDR による位置推定のためのクラス
     """
 
-    def estimate_pdr(self) -> Position | None:
-        """
-        PDR による位置推定を行う
-        Returns:
-            Position | None: 推定された位置、または推定できなかった場合は None
-        """
-        return None
+    def estimate_pdr(self) -> Position: ...
